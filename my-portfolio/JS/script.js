62ae55eac673b2ab84f427418cfa6a5a72a97d8b
huynhcongquanlaptrinh1 <quan70536@gmail.com>
2025-11-21 10:43:22 +0700

    Add files via upload

---
+++ my-portfolio/JS/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    console.log('Portfolio của Nguyễn Văn A đã load xong!');
+
+    alert('Chào mừng bạn đến với portfolio thật trên Internet của mình!');
+
+});
